05658d6ccbafb7f061b51aff42feca41afd4c748
Mahesh Shinde <maheshshinde028@gmail.com>
2024-07-02 19:43:32 +0530

    new changes

--- index.js
+++ index.js
@@ -3,32 +3,33 @@ const app = express();
 const cors = require('cors');
 const PORT = 3000;
 const bodyParser = require('body-parser');
-var path = require('path');
+const path = require('path');
 require('dotenv').config();
 
-app.use(express.json());
-
+// Apply CORS middleware with wildcard origin
 const corsOptions = {
-    origin: 'https://www.shankhanaadtrust.com', // Your frontend domain
+    origin: '*',
     optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
 
-// app.use(cors());
-app.use(
-    express.urlencoded({
-        extended: true
-    })
-);
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Serve static files from 'uploads' directory
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-//app.use(express.static('upload'));
 
+// Add logging middleware
+app.use((req, res, next) => {
+    console.log('Incoming request headers:', req.headers);
+    next();
+});
 
+// Define routes
 const usesrRouter = require('./routes/users');
-const adminRouter = require('./routes/admin')
+const adminRouter = require('./routes/admin');
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -37,6 +38,15 @@ app.get('/', (req, res) => {
 app.use('/users', usesrRouter);
 app.use('/admin', adminRouter);
 
+// Log response headers
+app.use((req, res, next) => {
+    res.on('finish', () => {
+        console.log('Response headers:', res.getHeaders());
+    });
+    next();
+});
+
+// Start the server
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
 });
